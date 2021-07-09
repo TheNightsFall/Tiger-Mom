b51c17768c56d000d226692923661941bd1c137f
Nights <63215794+TheNightsFall@users.noreply.github.com>
2021-07-09 17:09:19 -0500

    Task loop for Challenges

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ async def on_ready():
   #guildData.update_many({}, {"$set": {"queuelink": []}})
   #guildData.update_many({}, {"$set": {"queue": []}})
   checkDate.start()
-  #checkChal.start()
+  checkChal.start()
 
 @bot.event
 async def on_message(message):
@@ -77,11 +77,56 @@ async def checkDate():
       await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=" you practice | p.help"))
     await asyncio.sleep(5)
 
-@tasks.loop(seconds = 10)
+@tasks.loop(minutes = 1)
 async def checkChal():
   while True:
+    current = datetime.datetime.now()
     no = list(teamData.find({"accepted": {"$type": "array", "$ne": []}}))
-    await asyncio.sleep(5)
+    loop = -1
+    for x in no:
+      loop += 1
+      timeChal = datetime.datetime.strptime(no[loop]["accepted"][0]["expiration"], "%m/%d/%Y %H:%M:%S")
+      if current >= timeChal:
+        bal1 = no[loop]["accepted"][0]["bal"]
+        t1 = teamData.find_one({"tn": no[loop]["tn"]})
+        t2 = teamData.find_one({"tn": no[loop]["accepted"][0]["challenged"]})
+        capt1 = t1["captains"]
+        capt2 = t2["captains"]
+        bal2 = t2["accepted"][0]["bal"]
+        name1 = t1["tn"]
+        name2 = t2["tn"]
+        if bal1 == bal2:
+          if bal1 == 0:
+            xp1 = xp2 = 0
+            em1 = discord.Embed(title = "ATTENTION! CHALLENGE ENDED!", description = f"Hello, captains of {name1}! Your challenge against team {name2} has ended. Neither of you made any bubble tea. No xp rewarded.", color =	15417396)
+            em2 = discord.Embed(title = "ATTENTION! CHALLENGE ENDED!", description = f"Hello, captains of {name2}! Your challenge against team {name1} has ended. Neither of you made any bubble tea. No xp rewarded.", color =	15417396)
+          else:
+            xp1 = xp2 = 2
+            em1 = discord.Embed(title = "ATTENTION! CHALLENGE ENDED!", description = f"Hello, captains of {name1}! Your challenge against team {name2} has ended. You both tied, making {bal1}. You both gain 2 xp.", color =	15417396)
+            em2 = discord.Embed(title = "ATTENTION! CHALLENGE ENDED!", description = f"Hello, captains of {name2}! Your challenge against team {name1} has ended. You both tied, making {bal1}. You both gain 2 xp.", color =	15417396)
+        elif bal1 < bal2:
+          xp1 = 0
+          xp2 = 4
+          em1 = discord.Embed(title = "ATTENTION! CHALLENGE ENDED!", description = f"Hello, captains of {name1}! Your challenge against team {name2} has ended. You lost, {bal1} against {bal2}. Your team gains nothing.", color =	15417396)
+          em2 = discord.Embed(title = "ATTENTION! CHALLENGE ENDED!", description = f"Hello, captains of {name2}! Your challenge against team {name1} has ended. You won, {bal2} against {bal1}. Your team gains 4 xp.", color =	15417396)
+        else:
+          xp1 = 4
+          xp2 = 0
+          em2 = discord.Embed(title = "ATTENTION! CHALLENGE ENDED!", description = f"Hello, captains of {name2}! Your challenge against team {name1} has ended. You lost, {bal2} against {bal1}. Your team gains nothing.", color =	15417396)
+          em1 = discord.Embed(title = "ATTENTION! CHALLENGE ENDED!", description = f"Hello, captains of {name1}! Your challenge against team {name2} has ended. You won, {bal1} against {bal2}. Your team gains 4 xp.", color =	15417396)
+        teamData.update_one({"tn": name1}, {"$pull": {"accepted": {"expiration": no[loop]["accepted"][0]["expiration"], "challenged": name2, "bal": bal1}}})
+        teamData.update_one({"tn": name2}, {"$pull": {"accepted": {"expiration": no[loop]["accepted"][0]["expiration"], "challenged": name1, "bal": bal2}}})
+        teamData.update_one({"tn":name1}, {"$inc": {"xp": xp1}})
+        teamData.update_one({"tn":name2}, {"$inc": {"xp": xp2}})
+        for captain in capt1:
+            check = bot.get_user(int(captain))
+            channel = await check.create_dm()
+            await channel.send(embed = em1)
+        for captain in capt2:
+            check = bot.get_user(int(captain))
+            channel = await check.create_dm()
+            await channel.send(embed = em2)
+    await asyncio.sleep(50)
 
 if __name__ == "__main__":
   for extension in [f.replace('.py', '') for f in listdir("cogs") if isfile(join("cogs", f))]:
